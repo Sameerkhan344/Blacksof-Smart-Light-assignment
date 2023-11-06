0c663140844a1e9a86c2729ef57aae80d69b7e03
Sameerkhan344 <khansame34@gmail.com>
2023-11-06 11:53:10 +0530

    first commit

--- src/app/Components/SideNav/SideNav.tsx
+++ src/app/Components/SideNav/SideNav.tsx
@@ -28,7 +28,8 @@ const SideNav: React.FC = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover-text-blue-500 hover:bg-blue-700 dark:hover-text-white md:dark:hover-bg-transparenthover:text-white"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover-text-blue-500 hover:bg-blue-700 dark:hover-text-white md:dark:hover-bg-transparent
+                hover:text-white"
               >
                 Software Services
               </a>
