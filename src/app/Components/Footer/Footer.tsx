f822e91309c272ec6a89f17cecde94d6522829c8
Sameerkhan344 <khansame34@gmail.com>
2023-11-06 11:44:50 +0530

    first commit

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => {
                   className="h-8 mr-3"
                   alt="Flowbite Logo"
                 />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                   smart Lights
                 </span>
               </a>
